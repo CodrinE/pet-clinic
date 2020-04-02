d11092fb18aa20383bb44810545efd80758bad5b
CodrinE <ecodrin@yahoo.com>
2020-04-02 15:08:12 +0300

    created interfaces for Pet Clinic Services. close #5

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/OwnerService.java
@@ -0,0 +1,10 @@
+package com.springframework.petclinic.services;
+
+import com.springframework.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService extends CrudService<Owner, Long>{
+
+    Owner findByLastName(String lastName);
+}
