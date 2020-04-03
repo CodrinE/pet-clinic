ed74f03fe98a16b9d3928dbab49cccab341bfdf0
CodrinE <ecodrin@yahoo.com>
2020-04-03 18:44:26 +0300

    implemented method for listing all owners in idex page. Closes #15

--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.springframework.petclinic.controllers;
 
+import com.springframework.petclinic.services.OwnerService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/owners")
+@AllArgsConstructor
 public class OwnerController {
 
-    @RequestMapping({"/owners"})
-    public String listVets(){
+    private final OwnerService ownerService;
+
+    @RequestMapping({"", "/"})
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 
 }
+
