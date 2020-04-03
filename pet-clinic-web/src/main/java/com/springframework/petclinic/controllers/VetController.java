931685da9e395e1d57a0a72c8abe986cd43a5ddf
CodrinE <ecodrin@yahoo.com>
2020-04-03 18:56:53 +0300

    implemented method for listing all vets in idex page and removed vets/owners thymeleaf not save operator Closes #14

--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/VetController.java
@@ -1,13 +1,21 @@
 package com.springframework.petclinic.controllers;
 
+import com.springframework.petclinic.services.VetService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
+@RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"/vets"})
-    public String listVets(){
+    private final VetService vetService;
+
+    @RequestMapping({"", "/"})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
